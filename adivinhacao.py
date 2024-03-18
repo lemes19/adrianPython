ec02b4f0cef82b75b8ee0a5960791d7ef8041d55
lemes19 <adrian.lemes.machado@escola.pr.gov.br>
2024-03-18 10:53:55 -0300

    alteraca

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,7 +1,13 @@
 import random
 
+def dica(sorteio):
+    flutuante = sorteio /10
+    inteiro = int (flutuante)
+    dica_menor = inteiro*10
+    dica_maior = (inteiro +1)*10
+    return dica_menor, dica_maior
 def jogar():
-    max_rand = 10
+    
     print("####### BEM VINDO AO JOGO DA ADIVINHAÇÃO #######")
     sorteio = random.randint(0, max_rand)
     ## para criar dificultades: perguntar pro usuário (fácil, médio ou difícil)
@@ -10,21 +16,28 @@ def jogar():
     ## dificil = 3 tentativas (0- 50)
 
 
-    dificuldades = int(input("Digite a operação desejada:\n fácil \n médio \n dificil\n"))
+    dificuldades = int(input("Digite a operação desejada:\n 1-fácil \n 2-médio \n 3-dificil\n"))
 
     if dificuldades == 1 :
         limite_tentativas = 10
+        max_rand = 10
     elif dificuldades == 2 :
         limite_tentativas = 5
+        max_rand = 10
     elif dificuldades == 3 :
         limite_tentativas = 3
-    
+        max_rand = 100
+    else:
         print("Operação inválida!")
+
+    sorteio = random.randint(0, max_rand)
 
     tentativa = 1
     
     print("Tente adivinhar o número de 0 a 10, em {} tentativas!".format(limite_tentativas))
     while (tentativa <= limite_tentativas) :
+        if(tentativa== limite_tentativas):
+            print("dica aqui")
         chute = int(input("Digite o valor do seu chute: \n"))
 
         acertou = chute == sorteio
